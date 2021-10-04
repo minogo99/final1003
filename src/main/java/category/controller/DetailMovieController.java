a485940a2e95ac814434f93e15d3080be6793f0e
seojonghee <91777776+seojonghee@users.noreply.github.com>
2021-10-04 17:56:53 +0900

    Update DetailMovieController.java

--- src/main/java/category/controller/DetailMovieController.java
+++ src/main/java/category/controller/DetailMovieController.java
@@ -9,6 +9,17 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+package category.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
 import category.model.CategoryBean;
 import category.model.CategoryDao;
 
@@ -22,10 +33,10 @@ public class DetailMovieController {
 	CategoryDao cdao;
 	
 	@RequestMapping(value=command,method=RequestMethod.GET)
-	public ModelAndView doAction(@RequestParam("num") int vnum) {
+	public ModelAndView doAction(@RequestParam("num") int num) {
 		System.out.println("1");
 		
-		CategoryBean cb=cdao.detailMovieView(vnum);
+		CategoryBean cb=cdao.detailMovieView(num);
 		List<CategoryBean> lists=cdao.detailMovieGenre(cb.getGenre());
 		
 		ModelAndView mav=new ModelAndView();
