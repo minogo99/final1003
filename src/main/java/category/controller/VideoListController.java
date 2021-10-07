1abb74985fa30458c5cb4580fc01aa065464692b
seojonghee <91777776+seojonghee@users.noreply.github.com>
2021-10-07 16:19:36 +0900

    1번

--- src/main/java/category/controller/VideoListController.java
+++ src/main/java/category/controller/VideoListController.java
@@ -1,21 +1,38 @@
 package category.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import category.detail.DetailDao;
+import category.model.CategoryBean;
+import category.model.CategoryDao;
+
 @Controller
 public class VideoListController {
 	
 	private final String command="/videoList.category";
-	private final String getPage="";
+	private final String getPage="videoList";
+	
+	@Autowired
+	CategoryDao cdao;
+	
+	@Autowired
+	DetailDao ddao;
 	
 	@RequestMapping(value=command,method = RequestMethod.GET)
 	public ModelAndView doAction() {
 		
-		ModelAndView mav=new ModelAndView();
+		List<CategoryBean> clists=cdao.selectAll();
 		
+		
+		ModelAndView mav=new ModelAndView();
+		mav.addObject("clists",clists);
+		mav.setViewName(getPage);
 		return mav;
 	}
 }
---
+++ src/main/webapp/WEB-INF/category/videoList.jsp
@@ -0,0 +1,5 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@include file="../admin/display/top.jsp" %>
+<%@include file="./../common/common.jsp"%>
+videolist.jsp<br><br>
