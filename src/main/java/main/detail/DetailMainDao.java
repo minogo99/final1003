6a95d1620d4d82b99eb594312371aa53d3456f99
yuseunghwan <vkfkdrhfo@gmail.com>
2021-10-07 19:55:00 +0900

    detailMain

--- src/main/java/main/detail/DetailMainDao.java
+++ src/main/java/main/detail/DetailMainDao.java
@@ -6,23 +6,23 @@ import java.util.List;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 
-
 public class DetailMainDao {
 
-	String namespace="main.detail.DetailMainBean";
+	String namespace ="main.detail.DetailMainBean";
 	
 	@Autowired
 	SqlSessionTemplate sqlSessionTemplate;
-	
+
 	public DetailMainBean detailMainVideoView(int num) {
-		DetailMainBean dmb = sqlSessionTemplate.selectOne(namespace+".detailVideoView",num);
+		DetailMainBean dmb = sqlSessionTemplate.selectOne(namespace+".detailMainVideoView",num);
 		return dmb;
 	}
 
 	public List<DetailMainBean> detailMainVideoGenre(String genre) {
-		List<DetailMainBean> lists=new ArrayList<DetailMainBean>();
-		lists=sqlSessionTemplate.selectList(namespace+".detailVideoGenre", genre);
+		List<DetailMainBean> lists = new ArrayList<DetailMainBean>();
+		lists=sqlSessionTemplate.selectList(namespace+".detailMainVideoGenre",genre);
 		return lists;
 	}
-
+	
+	
 }
