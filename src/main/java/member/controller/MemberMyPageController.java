860cdd5d734fb120a05d30ebf30819d4bc85138c
dongincheon <dnjstjr1724@gmail.com>
2021-10-07 17:33:00 +0900

    a

--- src/main/java/member/controller/MemberMyPageController.java
+++ src/main/java/member/controller/MemberMyPageController.java
@@ -1,33 +1,29 @@
 package member.controller;
 
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-
 import member.model.MemberBean;
 import member.model.MemberDao;
 
 @Controller
-public class MemberDetailController {
-	private final String command = "/detail.admin";
-	private final String getPage = "memberDetailView";
-
+public class MemberMyPageController {
+	private final String command = "/mypage.member";
+	private final String getPage = "memberMypage";
+	
+	@Autowired
 	MemberDao memberDao;
 	
 	@RequestMapping(value=command)
-	public String doAction(@RequestParam("num")int num,
-			 @RequestParam("pageNumber") int pageNumber,
+	public String doAction(@RequestParam("num")int num,			 
 			 Model model) {
 		
 		MemberBean member = memberDao.getMember(num);
 		model.addAttribute("member", member);
-		model.addAttribute("pageNumber", pageNumber);
-		
-		
-		
+		System.out.println("마이페이지");		
 		return getPage;
 	}
 }
