97abc7aa6749d26610795fcfda7f62fea4a582af
jeongjiwoo <bksj04@naver.com>
2021-10-15 16:26:45 +0900

    시청내역
    시청내역

--- src/main/java/member/model/MemberWatchBean.java
+++ src/main/java/member/model/MemberWatchBean.java
@@ -5,6 +5,9 @@ public class MemberWatchBean {
 	private int num;
 	private String movie_num;
 	private String user_id;
+	private String watch_date;
+	
+	private String image;
 	
 	public int getNum() {
 		return num;
@@ -24,14 +27,29 @@ public class MemberWatchBean {
 	public void setUser_id(String user_id) {
 		this.user_id = user_id;
 	}
-	public MemberWatchBean(int num, String movie_num, String user_id) {
+	public String getWatch_date() {
+		return watch_date;
+	}
+	public void setWatch_date(String watch_date) {
+		this.watch_date = watch_date;
+	}
+	public String getImage() {
+		return image;
+	}
+	public void setImage(String image) {
+		this.image = image;
+	}
+	public MemberWatchBean(int num, String movie_num, String user_id, String watch_date, String image) {
 		super();
 		this.num = num;
 		this.movie_num = movie_num;
 		this.user_id = user_id;
+		this.watch_date = watch_date;
+		this.image = image;
 	}
 	public MemberWatchBean() {
 		super();
+		// TODO Auto-generated constructor stub
 	}
 	
 	
